09314537994324c0756e4c1e538f0f5242bb9d84
Shaurya <shaurya50211@gmail.com>
2022-07-27 14:33:01 -0400

    Initial Commit

---
+++ Quotify/QuotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotifyApp.swift
+//  Quotify
+//
+//  Created by Shaurya Gupta on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct QuotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
